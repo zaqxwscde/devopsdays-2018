a7b38b42f61b95369535bef52cf84ea030bfdee1
Michelle Leu <michelle_leu@trend.com.tw>
2018-09-11 02:08:22 +0800

    update check_pr

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 import json
 import time
 import glob
+from urllib.request import urlretrieve
 
 import yaml
 from flask import Flask
@@ -13,27 +14,46 @@ from flask import render_template
 GITHUB_ACCESS_TOKEN = os.environ.get('GITHUB_ACCESS_TOKEN', '')
 
 app = Flask(__name__, static_url_path='/static')
+app.config['FREEZER_DESTINATION'] = 'docs'
+
 g = Github(GITHUB_ACCESS_TOKEN)
 
 def check_pr(pr):
+    print(pr)
     files = [file for file in pr.get_files()]
-    print(files)
+    
+    # check number of file
+    print(files) 
     if len(files) != 1:
         return False
 
+    # check file status
     print(files[0].status)
     if files[0].status != 'added':
         print(files[0].status)
         return False
 
+    # check filename
     login = pr.user.login 
-    filename = files[0].filename
+    filename = files[0].filename.replace("messages/", "")
     login_yml = "{login}.yml".format(login=login)
+    print(filename)
     print(login_yml)
-    print(filename.replace("messages/", ""))
-
-    if filename.replace("messages/", "") == login_yml:
+    if filename != login_yml:
         return False
+
+    # check yaml format
+    print(files[0].raw_url)
+    urlretrieve(files[0].raw_url, filename)
+    with open(filename, 'r') as stream:
+        try:
+            data = yaml.load(stream)
+            if not data.get('displayname') or not data.get('message'):
+                return False
+        except yaml.YAMLError as exc:
+            print(exc)
+            return False
+    os.remove(filename)
 
     return True
 
@@ -41,7 +61,7 @@ def check_pr(pr):
 def index():
     if request.method == 'POST':
         payload = json.loads(request.data)
-        if payload['action'] == 'opened' or payload['action'] == 'reopened':
+        if payload.get('action') and payload['action'] in ['opened', 'reopened']:
             repo = g.get_repo(payload['repository']['id'])
             pr_number = payload['number']
             pr = repo.get_pull(pr_number)
@@ -59,7 +79,8 @@ def index():
             try:
                 messages.append({
                     'message': data['message'],
-                    'display_name': data['displayname']
+                    'display_name': data['displayname'],
+                    'username': f.replace("messages/", "").replace(".yml", "")
                 })
             except yaml.YAMLError as exc:
                 print(exc)
